9be14b23390237acb0de827480e200dfc8f1a649
Your Name <you@example.com>
2018-12-14 03:07:10 +0200

    erwr

--- Back-End/eudoxus/src/main/java/main/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long>,JpaRepository<User,Long> {
-	public User findByEmail(String email);
-}
